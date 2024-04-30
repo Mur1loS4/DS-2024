fdab4c82b15df2e056c6737851d4bb3b16f2434b
Murilo Sousa <166508877+Mur1loS4@users.noreply.github.com>
2024-04-30 14:26:41 -0300

    Add files via upload

---
+++ Arquitetura de redes IoT/Atividades/avaliação formativa semafaro/main.py
@@ -0,0 +1,19 @@
+import machine
+import time
+
+verde = machine.Pin(27, machine.Pin.OUT)
+amarelo = machine.Pin(26, machine.Pin.OUT)
+vermelho = machine.Pin(25, machine.Pin.OUT)
+
+while True:
+    verde.value(1)
+    time.sleep(3)
+    verde.value(0)
+
+    amarelo.value(1)
+    time.sleep(1)
+    amarelo.value(0)
+
+    vermelho.value(1)
+    time.sleep(5)
+    vermelho.value(0)
